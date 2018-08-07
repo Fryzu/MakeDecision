195c414c55b74f7d0907e03302ab5d9f6bf92a0c
Fryzu <bartek.fryz@gmail.com>
2018-08-07 18:07:25 +0200

    question detail

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
-"""make_decision_project URL Configuration
+"""bsa_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,17 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, re_path
+from .views import *
 
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'', include("posts.urls")),
+    re_path(r'questions/(?P<requested_slug>[a-z0-9-]+)$', post_details, name='details'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import *
 
 # Create your views here.
+
+def post_details(request, requested_slug=None):
+    instance = get_object_or_404(Post, slug=requested_slug)
+    context = {
+        "post": instance,
+    }
+    return render(request, "post_details.html", context)
